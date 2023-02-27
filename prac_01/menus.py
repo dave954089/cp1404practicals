d0a2286b170d9320a142fce5f56dcedc3c22e3cf
Dave <davindersingh1@my.jcu.edu.au>
2023-02-27 22:46:04 +1000

    Use of function

--- prac_01/menus.py
+++ prac_01/menus.py
@@ -1,13 +1,22 @@
-name = input("Enter name: ")
-print("(H)ello\n(G)oodbye\n(Q)uit")
-choice = input(">>> ").upper()
-while choice != "Q":  # To select choices in menu
-    if choice == "H":
-        print("Hello", name)
-    elif choice == "G":
-        print("Goodbye", name)
-    else:
-        print("Invalid choice")
+def main():
+    name = input("Enter name: ")
     print("(H)ello\n(G)oodbye\n(Q)uit")
+    choice = choice_selection()
+    while choice != "Q":  # To select choices in menu
+        if choice == "H":
+            print("Hello", name)
+        elif choice == "G":
+            print("Goodbye", name)
+        else:
+            print("Invalid choice")
+        print("(H)ello\n(G)oodbye\n(Q)uit")
+        choice = choice_selection()
+    print("Finished.")
+
+
+def choice_selection():
     choice = input(">>> ").upper()
-print("Finished.")
+    return choice
+
+
+main()
