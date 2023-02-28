ba16585edc17d55332aa482ad998f36995a6d609
Dave <davindersingh1@my.jcu.edu.au>
2023-02-28 22:42:47 +1000

    TODO part

---
+++ prac_03/randoms.py
@@ -0,0 +1,20 @@
+import random
+
+print(random.randint(5, 20))  # line 1
+print(random.randrange(3, 10, 2))  # line 2
+print(random.uniform(2.5, 5.5))  # line 3
+# 8
+# smallest 5 and biggest 20.
+
+
+# 9
+# smallest 3 and biggest 9
+# no
+
+
+# 4.4364808442550645
+# smallest 2.5 and largest 5.5
+
+print(random.randint(1,100))
+
+
